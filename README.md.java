be22400981413ee371a9a80f1a19881795a38626
Francisco Cardoso <francisco.neto2903@gmail.com>
2025-07-27 11:43:12 -0300

    Update README.md.java

--- README.md.java
+++ README.md.java
@@ -16,7 +16,7 @@ public class Robot extends TimedRobot {
     private final VictorSPX m_rightDrive2 = new VictorSPX(1);
 
     double velocidadeD;
-    double velocidadeL;
+    double velocidadeE;
 
     Joystick bob = new Joystick(0);
 
@@ -28,6 +28,9 @@ public class Robot extends TimedRobot {
     double x_right;
     double y_left;
     double y_right;
+
+    double anaE;
+    double anaD;
 
     double m_speed = 0;
     int POV;
@@ -64,11 +67,11 @@ public class Robot extends TimedRobot {
     Ltrigger = bob.getRawAxis(2);
     Rtrigger = bob.getRawAxis(3);
 
-     x_left = bob.getRawAxis(0);
-     y_left = bob.getRawAxis(1);
+    x_left = bob.getRawAxis(0);
+    y_left = bob.getRawAxis(1);
 
-     x_right = bob.getRawAxis(4);
-     y_right = bob.getRawAxis(5);
+    x_right = bob.getRawAxis(4);
+    y_right = bob.getRawAxis(5);
 
 
     POV = bob.getPOV();
@@ -91,8 +94,8 @@ public class Robot extends TimedRobot {
 
     m_rightDrive.set(ControlMode.PercentOutput, velocidadeD);
     m_rightDrive2.set(ControlMode.PercentOutput, velocidadeD);
-    m_leftDrive.set(ControlMode.PercentOutput, velocidadeL);
-    m_leftDrive2.set(ControlMode.PercentOutput, velocidadeL);
+    m_leftDrive.set(ControlMode.PercentOutput, velocidadeE);
+    m_leftDrive2.set(ControlMode.PercentOutput, velocidadeE);
   }
 
   public void dash(){
@@ -100,10 +103,12 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("LTrigger", Ltrigger);
     SmartDashboard.putNumber("m_speed", m_speed);
     SmartDashboard.putNumber("velocidadeD", velocidadeD);
-    SmartDashboard.putNumber("VelocidadeL", velocidadeL);
+    SmartDashboard.putNumber("VelocidadeE", velocidadeE);
     SmartDashboard.putBoolean("a", a);
     SmartDashboard.putBoolean("b", b);
     SmartDashboard.putBoolean("x", x);
+    SmartDashboard.putNumber("Analógico Esquerdo", anaE);
+    SmartDashboard.putNumber("Analógico Direito", anaD);
 
   }
 
@@ -120,33 +125,33 @@ public class Robot extends TimedRobot {
   public void pov() {
     switch(POV){
       case 0:
-        velocidadeL = m_speed;
+        velocidadeE = m_speed;
         velocidadeD = m_speed;
         break;
       case 90: 
-        velocidadeL = m_speed;
+        velocidadeE = m_speed;
         velocidadeD = -m_speed;
         break;
       case 180:
-        velocidadeL = -m_speed;
+        velocidadeE = -m_speed;
         velocidadeD = -m_speed;
         break;
       case 270:
-        velocidadeL = -m_speed;
+        velocidadeE = -m_speed;
         velocidadeD = m_speed;
         break;
       case -1:
-        velocidadeL = 0;
+        velocidadeE = 0;
         velocidadeD = 0;
       }
   }
  
   public void Ltrigger(){
     if (Ltrigger > 0.04){
-      velocidadeL = Ltrigger * -m_speed;
+      velocidadeE = Ltrigger * -m_speed;
       velocidadeD = Ltrigger * -m_speed;
     } else{
-      velocidadeL = m_speed * 0;
+      velocidadeE = m_speed * 0;
       velocidadeD = m_speed * 0;
     }
   }
@@ -154,29 +159,32 @@ public class Robot extends TimedRobot {
   public void Rtrigger(){
     if (Rtrigger > 0.04){
       velocidadeD = Rtrigger * m_speed;
-      velocidadeL = Rtrigger * m_speed;
+      velocidadeE = Rtrigger * m_speed;
     } else{
       velocidadeD = m_speed * 0;
-      velocidadeL = m_speed * 0;
+      velocidadeE = m_speed * 0;
     }
   }
 
   public void anaE() {
     if (y_left > 0 & x_left > 0){ //primeiro quadrante
       velocidadeD = magnitude * m_speed - 1;
-      velocidadeL = magnitude * m_speed + 1;
+      velocidadeE = magnitude * m_speed + 1;
       }
     else if (y_left > 0 & x_left < 0){ //segundo quadrante
       velocidadeD = magnitude * m_speed + 1;
-      velocidadeL = magnitude * m_speed - 1;
+      velocidadeE = magnitude * m_speed - 1;
       }
     else if (y_left < 0 & x_left < 0){ //terceiro quadrante
       velocidadeD = magnitude * m_speed - 1;
-      velocidadeL = magnitude * m_speed + 1;
+      velocidadeE = magnitude * m_speed + 1;
       }
     else if (y_left < 0 & x_left > 0){ //quarto quadrante
       velocidadeD = magnitude * m_speed + 1;
-      velocidadeL = magnitude * m_speed - 1;
+      velocidadeE = magnitude * m_speed - 1;
+      } else {
+        velocidadeD = 0;
+        velocidadeE = 0;
       }
     }
   // public void anaD(){
